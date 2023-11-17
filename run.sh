7023fcb57e65cfbf49016881960a991bbe2079d5
quangtuyen <tuyen.tranquang@ascendcorp.com>
2023-11-17 17:06:51 +0800

    add files

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
-CONTAINERNAME=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
+CONTAINER_ID=`docker ps | egrep 'availj/avail' | awk '{print $1}'`
+
+docker rename CONTAINER_ID avail
 
 wget -O entrypoint.sh https://raw.githubusercontent.com/KenSukiVN/avail/main/entrypoint.sh
+chmod +x entrypoint.sh
 
-docker  cp entrypoint.sh ${CONTAINERNAME}:/entrypoint.sh
+docker  cp entrypoint.sh ${CONTAINER_ID}:/entrypoint.sh
 
-docker restart ${CONTAINERNAME}
+docker restart avail
 
-docker logs -f --tail 100 ${CONTAINERNAME}
+docker logs -f --tail 100 avail
